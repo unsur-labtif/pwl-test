f230f0e9124782f681c185deaeca2515d99297fd
PWL-5 <labtif.unsur@gmail.com>
2023-10-23 13:08:45 +0700

    pertemuan 4

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,42 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
     public function index() {
-        $data['mahasiswa'] = [
-            [
-                'nama' => 'Mahasiswa 1',
-                'npm' => '552012001'
-            ],
-            [
-                'nama' => 'Mahasiswa 2',
-                'npm' => '552012002'
-            ]
-        ];
-
-        return view('mahasiswa.index',)->with($data);
+        $data['mahasiswa'] = Mahasiswa::all();
+        return view('mahasiswa.index' )->with($data);
     }
 
     public function edit($npm) {
-        $data = [
-            [
-                'nama' => 'Mahasisawa 1',
-                'npm' => '552012001'
-            ],
-            [
-                'nama' => 'Mahasisawa 2',
-                'npm' => '552012002'
-            ]
-        ];
-        $dataMhs = null;
-        foreach($data as $key => $mhs){
-            if($mhs['npm'] == $npm){
-                $dataMhs = $data[$key];
-            }
-        }
+       $dataMhs = Mahasiswa::where('npm', $npm)->first();
 
         if($dataMhs == null){
             return redirect()->route('mahasiswa.index');
---
+++ database/migrations/2023_10_21_086950_modify_mahasiswas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_prodi');
+            $table->foreign('id_prodi')->references('id')->on('prodis')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            //
+        });
+    }
+};
